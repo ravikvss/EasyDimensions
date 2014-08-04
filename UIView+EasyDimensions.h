131be93e4637b2164c4aa8eb64b694ffd2933d1f
Ravi Kondapalli <kvss.ravi@gmail.com>
2014-08-04 12:28:40 -0700

    Added 1st version of EasyDimensions

---
+++ UIView+EasyDimensions.h
@@ -0,0 +1,26 @@
+//
+//  UIView+EasyDimensions.h
+//
+//  Created by Ravi Kondapalli on 8/4/14.
+//  Copyright (c) 2014 Ravi Kondapalli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (EasyDimensions)
+-(void)setFrameWithNewX:(CGFloat)newX;
+-(void)setFrameWithNewY:(CGFloat)newY;
+-(void)setFrameWithNewWidth:(CGFloat)newWidth;
+-(void)setFrameWithNewHeight:(CGFloat)newHeight;
+-(void)setFrameWithNewOrigin:(CGPoint)newOrigin;
+-(void)setFrameWithNewSize:(CGSize)newSize;
+
+-(void)setFrameByShiftingOrigin:(CGPoint)originShift;
+-(void)setFrameByShiftingX:(CGFloat)xShift;
+-(void)setFrameByShiftingY:(CGFloat)yShift;
+
+-(CGFloat)x__;
+-(CGFloat)y__;
+-(CGFloat)width__;
+-(CGFloat)height__;
+@end
---
+++ UIView+EasyDimensions.m
@@ -0,0 +1,76 @@
+//
+//  UIView+EasyDimensions.m
+//
+//  Created by Ravi Kondapalli on 8/4/14.
+//  Copyright (c) 2014 Ravi Kondapalli. All rights reserved.
+//
+
+#import "UIView+EasyDimensions.h"
+
+@implementation UIView (EasyDimensions)
+-(void)setFrameWithNewX:(CGFloat)newX
+{
+  self.frame = CGRectMake(newX, self.frame.origin.y, self.frame.size.width, self.frame.size.height);
+}
+
+-(void)setFrameWithNewY:(CGFloat)newY
+{
+  self.frame = CGRectMake(self.frame.origin.x, newY, self.frame.size.width, self.frame.size.height);
+}
+
+-(void)setFrameWithNewWidth:(CGFloat)newWidth
+{
+  self.frame = CGRectMake(self.frame.origin.x, self.frame.origin.y, newWidth, self.frame.size.height);
+}
+
+-(void)setFrameWithNewHeight:(CGFloat)newHeight
+{
+  self.frame = CGRectMake(self.frame.origin.x, self.frame.origin.y, self.frame.size.width, newHeight);
+}
+
+-(void)setFrameWithNewOrigin:(CGPoint)newOrigin
+{
+  self.frame = CGRectMake(newOrigin.x, newOrigin.y, self.frame.size.width, self.frame.size.height);
+}
+
+-(void)setFrameWithNewSize:(CGSize)newSize
+{
+  self.frame = CGRectMake(self.frame.origin.x, self.frame.origin.y, newSize.width, newSize.height);
+}
+
+-(void)setFrameByShiftingOrigin:(CGPoint)originShift
+{
+  self.frame = CGRectMake(self.frame.origin.x + originShift.x, self.frame.origin.y + originShift.y, self.frame.size.width, self.frame.size.height);
+}
+
+-(void)setFrameByShiftingX:(CGFloat)xShift
+{
+  self.frame = CGRectMake(self.frame.origin.x + xShift, self.frame.origin.y, self.frame.size.width, self.frame.size.height);
+}
+
+-(void)setFrameByShiftingY:(CGFloat)yShift
+{
+  self.frame = CGRectMake(self.frame.origin.x, self.frame.origin.y + yShift, self.frame.size.width, self.frame.size.height);
+}
+
+-(CGFloat)width__
+{
+  return self.frame.size.width;
+}
+
+-(CGFloat)x__
+{
+  return self.frame.origin.x;
+}
+
+-(CGFloat)y__
+{
+  return self.frame.origin.y;
+}
+
+-(CGFloat)height__
+{
+  return self.frame.size.height;
+}
+
+@end
